4873480ece9bda9a33cbf7ac28aead17371c8513
Roger Taracha <rojtaracha@gmail.com>
2019-10-28 14:55:15 +0300

    Check restrictions: URL {+} Create the Restrictions Manager object & define in it onCreate() method.
    {+} Check the app restrictions in the OnResume method.

--- app/src/main/java/com/example/thedancercodes/myenterpriseapplication/ImageActivity.java
+++ app/src/main/java/com/example/thedancercodes/myenterpriseapplication/ImageActivity.java
@@ -1,7 +1,10 @@
 package com.example.thedancercodes.myenterpriseapplication;
 
+import android.content.Context;
+import android.content.RestrictionsManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.URLUtil;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -12,21 +15,40 @@ public class ImageActivity extends AppCompatActivity {
 
     ImageView fullScreenImage;
 
+    // Create the Restrictions Manager object
+    RestrictionsManager myRestrictionsManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
 
         fullScreenImage = findViewById(R.id.fullScreenImage);
+
+        myRestrictionsManager = (RestrictionsManager) getSystemService(Context.RESTRICTIONS_SERVICE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        Glide.with(getApplicationContext())
-                .load(imageUrl)
-                .centerCrop()
-                .into(fullScreenImage);
+        // Check the app restrictions.
+        Bundle applicationRestrictions = myRestrictionsManager.getApplicationRestrictions();
+
+        // Check that the Bundle has content
+        if (applicationRestrictions.size() > 0) {
+
+            // Set new image URL
+            imageUrl = applicationRestrictions.getString("imageUrl");
+
+            // Checking that the URL is valid
+            if (URLUtil.isValidUrl(imageUrl)) {
+
+                Glide.with(getApplicationContext())
+                        .load(imageUrl)
+                        .centerCrop()
+                        .into(fullScreenImage);
+            }
+        }
     }
 }
